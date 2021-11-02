aaa246a155839ca303de55f39544032a26efcae8
HazemMeqdad <hazemmeqdad@gmail.com>
2021-11-03 00:58:28 +0200

    source code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# tasks
+# Task
+
+A simple package to make async task backgrond
+
+## Example
+
+```py
+import tasks
+
+async def printer(text: str):
+    print(text)
+ 
+task = tasks.Task(printer, intervel=2)  # function, timer 
+task.start(text="lol")
+```
+
+*if you use my package please give me start 😭😭 
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tasks",
+    version="0.0.1",
+    author="HazemMeqdad",
+    author_email="hazemmeqdad@gmail.com",
+    description="A simple package to make async task background",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DwcTeam/tasks",
+    project_urls={
+        "Bug Tracker": "https://github.com/DwcTeam/tasks/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
