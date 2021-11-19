d4b3d059dc520cf830281b76cc9bf9775d5d919c
HazemMeqdad <hazemmeqdad@gmail.com>
2021-11-19 20:52:04 +0200

    remove run_forever() add move files

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -3,34 +3,33 @@ import asyncio
 
 __all__ = ("Task")
 
+
 class Task:
     """
     Create loop task to run forever
     to start call start(*args, **kwargs)
     """
+
     def __init__(self, core, *, intervel: int | float) -> None:
-        self.core = core 
+        self.core = core
         self.intervel = intervel
         self.loop = asyncio.get_event_loop()
         self._task: asyncio.Task[None] = None
         self._stats: bool = True
-        self.result = None 
-    
+        self.result = None
+
     async def _loop(self, *args, **kwargs):
         while self._stats:
             self.result = await self.core(*args, **kwargs)
             await asyncio.sleep(self.intervel)
-    
+
     def start(self, *args, **kwargs) -> None:
         """
         To start task
         """
         task = self.loop.create_task(self._loop(*args, **kwargs))
         self._task = task
-        try:
-            self.loop.run_forever()
-        except KeyboardInterrupt: ...
-    
+
     def cancel(self) -> None:
         """
         To stop task
