0a336b968629083c3f77fc156a06c9ecb296ca15
HazemMeqdad <hazemmeqdad@gmail.com>
2021-11-19 20:54:07 +0200

    set package name :سخﻻ:

--- setup.py
+++ setup.py
@@ -21,6 +21,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=["tasks"],
-    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
